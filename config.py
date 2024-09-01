f455f3c423f4fe9e5b99dc601c2fe129cfc8ce73
aditya9507486180 <firefox95074.8@gmail.com>
2024-09-01 14:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7427052085:AAGHBfZ92GuLI47szb_86hVV0kAxEUY_t2w")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("ADITYAx09")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6503409388")
 
 
 
